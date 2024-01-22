67b99b9f5b6420df809fe9140bd06d6a0995a4a0
Agnes <agnesgay@gmail.com>
2024-01-22 14:50:13 +0100

    add base files

---
+++ src/main/java/mascarade/mascaradebackend/MascaradeBackEndApplication.java
@@ -0,0 +1,13 @@
+package mascarade.mascaradebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MascaradeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MascaradeBackEndApplication.class, args);
+	}
+
+}
