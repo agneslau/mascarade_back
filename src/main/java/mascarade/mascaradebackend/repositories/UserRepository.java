aa23fea89fe4d5d3a2cf05b8b7a45178815b8460
Agnes <agnesgay@gmail.com>
2024-02-06 15:50:16 +0100

    add : - authent works but not roles

--- src/main/java/mascarade/mascaradebackend/repositories/UserRepository.java
+++ src/main/java/mascarade/mascaradebackend/repositories/UserRepository.java
@@ -4,8 +4,11 @@ import mascarade.mascaradebackend.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, UUID> {
+
+    public Optional<User> findByEmail(String email);
 }
---
+++ src/main/java/mascarade/mascaradebackend/services/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,56 @@
+package mascarade.mascaradebackend.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import mascarade.mascaradebackend.dtos.AuthenticationRequest;
+import mascarade.mascaradebackend.dtos.AuthenticationResponse;
+import mascarade.mascaradebackend.dtos.RegisterRequest;
+import mascarade.mascaradebackend.entities.User;
+import mascarade.mascaradebackend.repositories.UserRepository;
+import mascarade.mascaradebackend.security.Jwt.JwtService;
+import mascarade.mascaradebackend.security.Role;
+import mascarade.mascaradebackend.services.AuthenticationService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    @Override
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        var user = User.builder()
+                .name(registerRequest.getName())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.ROLE_ADMIN)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    @Override
+    public AuthenticationResponse authenticate(AuthenticationRequest registerRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        registerRequest.getEmail(),
+                        registerRequest.getPassword())
+        );
+        var user = repository.findByEmail(registerRequest.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+
+    }
+}
