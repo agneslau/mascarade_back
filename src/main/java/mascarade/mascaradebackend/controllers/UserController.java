c8777ac4c49a17d2d850aeea0068485941be3e55
Agnes <agnesgay@gmail.com>
2024-03-04 14:51:07 +0100

    add : - edit user

--- src/main/java/mascarade/mascaradebackend/controllers/UserController.java
+++ src/main/java/mascarade/mascaradebackend/controllers/UserController.java
@@ -4,6 +4,7 @@ package mascarade.mascaradebackend.controllers;
 import mascarade.mascaradebackend.dtos.UserDto;
 import mascarade.mascaradebackend.security.Role;
 import mascarade.mascaradebackend.services.impl.UserServiceImpl;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -46,6 +49,15 @@ public class UserController {
     public ResponseEntity<Void> deleteUser(@PathVariable String id){
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    public ResponseEntity<UserDto> updateUser(@PathVariable String id, @RequestBody UserDto userDto){
+        if(!id.equals(userDto.id())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id in path and body do not match");
+        }
+        return ResponseEntity.ok(userService.updateUser(userDto));
     }
 
     @GetMapping("/name/{name}")
